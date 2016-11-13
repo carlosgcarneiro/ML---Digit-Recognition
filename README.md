9094fdda296afabaf03794bfd8c5acfd52dd32e8
Carlos Augusto Guerra Carneiro <carlosgcarneyro@gmail.com>
2016-11-13 19:08:58 -0200

    trying to do regression logistic work...

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ML - Digit-Recognition
 ### First run (to prepere the data):
-#### python preparedata.py $size_of_data$
+#### python preparedata.py $size_of_data$ $file_to_prepare$
+Ex to prepare *train* file: python preparedata.py 1000
+
 ### Later run:
 #### python run_methods.py
--- run_methods.py
+++ run_methods.py
@@ -15,41 +15,32 @@ with open('prepared_data.csv', 'rb') as f:
     #data=[tuple(line) for line in csv.reader(f)]
 d = np.array(data)
 
-
 np.random.shuffle(d) # shuffle the data to train
 
 h = .02  # step size in the mesh
 
 logreg = linear_model.LogisticRegression(C=1e5)
 
+X = d[:,1:]
+Y = d[:,0]
+print ("Fitting...")
 # we create an instance of Neighbours Classifier and fit the data.
+
 logreg.fit(X, Y)
 
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = X[:, 1:].min() - .5, X[:, 1:].max() + .5
-y_min, y_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
-
-print Y[0],Z[0]
-
+print ("Reading test...")
+with open('test.csv', 'rb') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    #data=[tuple(line) for line in csv.reader(f)]
+	
+X_test = np.array(data)
 
 
+for i in range(1,len(X_test)):
+	X_test[i] = map(int, X_test[i])	
+	
+Z = logreg.predict(examples)
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure(1, figsize=(4, 3))
-plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+print Z
 
-# Plot also the training points
-plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
